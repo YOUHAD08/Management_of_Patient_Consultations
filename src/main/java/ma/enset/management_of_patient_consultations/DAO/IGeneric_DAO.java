0049df426a6f922c3218b0e93d82e050489d977d
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-03-08 00:11:07 +0000

    feat: Implement database operations in Patient_DAO
    - Added `Create` method to insert patient records into the database.
    - Added `Update` method to modify existing patient records.
    - Added `Delete` method to remove patient records by ID.
    - Implemented `getAll` method to retrieve all patients from the database.
    - Implemented `getById` method to fetch a patient by ID.
    - Used `DBConnection` for database connectivity.

--- src/main/java/ma/enset/management_of_patient_consultations/DAO/IGeneric_DAO.java
+++ src/main/java/ma/enset/management_of_patient_consultations/DAO/IGeneric_DAO.java
@@ -1,11 +1,12 @@
 package ma.enset.management_of_patient_consultations.DAO;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IGeneric_DAO<E,U>{
-    void Create(E e);
-    void Update(E e);
-    void Delete(E e);
-    List<E> getAll();
-    E getById(U id);
+    void Create(E e) throws SQLException;
+    void Update(E e) throws SQLException;
+    void Delete(E e) throws SQLException;
+    List<E> getAll() throws SQLException;
+    E getById(U id) throws SQLException;
 }
--- src/main/java/ma/enset/management_of_patient_consultations/DAO/Patient_DAO.java
+++ src/main/java/ma/enset/management_of_patient_consultations/DAO/Patient_DAO.java
@@ -2,31 +2,77 @@ package ma.enset.management_of_patient_consultations.DAO;
 
 import ma.enset.management_of_patient_consultations.entities.Patient;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Patient_DAO implements IPatient_DAO {
     @Override
-    public void Create(Patient patient) {
-
+    public void Create(Patient patient) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedstatement = connection.prepareStatement("INSERT INTO PATIENTS(First_Name,Last_Name, Tel,Address)" +
+                "VALUES (?,?,?,?)");
+        preparedstatement.setString(1, patient.getFirst_Name());
+        preparedstatement.setString(2, patient.getLast_Name());
+        preparedstatement.setString(3, patient.getTel());
+        preparedstatement.setString(4, patient.getAddress());
+        preparedstatement.executeUpdate();
     }
 
     @Override
-    public void Update(Patient patient) {
-
+    public void Update(Patient patient) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedstatement = connection.prepareStatement("UPDATE PATIENTS SET First_Name=?,Last_Name=?, Tel=?, Address=? WHERE PATIENT_ID=?");
+        preparedstatement.setString(1, patient.getFirst_Name());
+        preparedstatement.setString(2, patient.getLast_Name());
+        preparedstatement.setString(3, patient.getTel());
+        preparedstatement.setString(4, patient.getAddress());
+        preparedstatement.setLong(5, patient.getPatient_ID());
     }
 
     @Override
-    public void Delete(Patient patient) {
-
+    public void Delete(Patient patient) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedstatement = connection.prepareStatement("DELETE FROM PATIENTS WHERE PATIENT_ID=?");
+        preparedstatement.setLong(1, patient.getPatient_ID());
+        preparedstatement.executeUpdate();
     }
 
     @Override
-    public List<Patient> getAll() {
-        return List.of();
+    public List<Patient> getAll() throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedstatement = connection.prepareStatement("SELECT * FROM PATIENTS ");
+        ResultSet resultSet = preparedstatement.executeQuery();
+        List<Patient> patients = new ArrayList<>();
+        while (resultSet.next()) {
+            Patient patient = new Patient();
+            patient.setPatient_ID(resultSet.getLong("PATIENT_ID"));
+            patient.setFirst_Name(resultSet.getString("First_Name"));
+            patient.setLast_Name(resultSet.getString("Last_Name"));
+            patient.setTel(resultSet.getString("Tel"));
+            patient.setAddress(resultSet.getString("Address"));
+            patients.add(patient);
+        }
+        return patients ;
     }
 
     @Override
-    public Patient getById(Long id) {
-        return null;
+    public Patient getById(Long id) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedstatement = connection.prepareStatement("SELECT * FROM PATIENTS WHERE PATIENT_ID=?");
+        preparedstatement.setLong(1, id);
+        ResultSet resultSet = preparedstatement.executeQuery();
+        Patient patient = new Patient();
+        if (resultSet.next()) {
+            patient.setPatient_ID(resultSet.getLong("PATIENT_ID"));
+            patient.setFirst_Name(resultSet.getString("First_Name"));
+            patient.setLast_Name(resultSet.getString("Last_Name"));
+            patient.setTel(resultSet.getString("Tel"));
+            patient.setAddress(resultSet.getString("Address"));
+        }
+        return patient;
     }
 }
